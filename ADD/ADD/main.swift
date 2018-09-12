9640989967ad9e3a33f7c7dd1ebc509fd35f2340
20161104615 <20161104615a@admindeiMac-9.local>
2018-09-12 11:28:11 +0800

    Update

---
+++ ADD/ADD/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  ADD
+//
+//  Created by 20161104615 on 2018/9/12.
+//  Copyright © 2018年 20161104615. All rights reserved.
+//
+
+import Foundation
+
+var i = 0
+var sum = 0
+
+for i in 1...100{
+    sum = sum + i
+}
+print(sum)
+
