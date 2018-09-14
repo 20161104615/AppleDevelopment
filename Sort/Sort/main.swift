3ea2783ce958aee52d53bb1da5c4339650265fbd
20161104615 <20161104615a@admindeiMac-9.local>
2018-09-14 17:32:43 +0800

    Sort

---
+++ Sort/Sort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sort
+//
+//  Created by 20161104615 on 2018/9/14.
+//  Copyright © 2018年 20161104615. All rights reserved.
+//
+
+import Foundation
+
+let someA = [3,4,1,5,6,2]
+
