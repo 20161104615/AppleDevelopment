777e2aaee90e3d8234ae3c5f0c0cc0e76ea58689
20161104615 <20161104615a@admindeiMac-9.local>
2018-09-26 10:58:00 +0800

    Calculator

---
+++ Calculator/Calculator1/Calculator1/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by 20161104615 on 2018/9/26.
+//  Copyright © 2018年 20161104615. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func Key1(_ sender: Any) {
+    }
+    @IBAction func Key2(_ sender: Any) {
+    }
+    @IBAction func Key3(_ sender: Any) {
+    }
+    @IBAction func Key4(_ sender: Any) {
+    }
+    @IBAction func Key5(_ sender: Any) {
+    }
+    @IBAction func Key6(_ sender: Any) {
+    }
+    @IBAction func Key7(_ sender: Any) {
+    }
+    @IBAction func Key8(_ sender: Any) {
+    }
+    @IBAction func Key9(_ sender: Any) {
+    }
+    @IBAction func Key0(_ sender: Any) {
+    }
+    @IBAction func Equal(_ sender: Any) {
+    }
+    @IBAction func Add(_ sender: Any) {
+    }
+    @IBAction func Minus(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FastSorting/FastSorting/FastSorting/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  FastSorting
+//
+//  Created by 20161104615 on 2018/9/19.
+//  Copyright © 2018年 20161104615. All rights reserved.
+//
+
+import Foundation
+
+var tuple = ("A",20,"man",2007,"B",21,"wuman",2009)
+
+
+
+
+
