0fa8cb884c65e4602b9e3fd97153b7a5920a8702
20161104615 <20161104615a@admindeiMac-9.local>
2018-09-28 17:12:06 +0800

    20180928Update

--- Calculator/Calculator1/Calculator1/ViewController.swift
+++ Calculator/Calculator1/Calculator1/ViewController.swift
@@ -9,12 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var Sum = Int()
     
-    
-    @IBOutlet weak var Result: UILabel!
+    @IBOutlet weak var Result: UITextField!
     var DisplayWindowAcknowledgement = false//表示是否是第一次输入数字
-
     @IBAction func key_1(sender : AnyObject) {
         let key = sender.currentTitle
         if (DisplayWindowAcknowledgement){
@@ -25,45 +22,118 @@ class ViewController: UIViewController {
                 DisplayWindowAcknowledgement = true
             }
         }
-        
     }
-    @IBAction func key_2(_ sender: Any) {
+    @IBAction func key_2(sender: AnyObject) {
     //Result.text = "2"
-    }
-    @IBAction func key_3(_ sender: Any) {
-    //Result.text = "3"
-    }
-    @IBAction func key_4(_ sender: Any) {
-    //Result.text = "4"
-    }
-    @IBAction func key_5(_ sender: Any) {
-    //Result.text = "5"
-    }
-    @IBAction func key_6(_ sender: Any) {
-    //Result.text = "6"
-    }
-    @IBAction func key_7(_ sender: Any) {
-    //Result.text = "7"
-    }
-    @IBAction func key_8(_ sender: Any) {
-    //Result.text = "8"
-    }
-    @IBAction func key_9(_ sender: Any) {
-    //Result.text = "9"
-    }
-    @IBAction func key_0(_ sender: Any) {
-    //Result.text = "0"
-    }
-    
-    
-    @IBAction func Equal(_ sender: Any) {
-        switch <#value#> {
-        case <#pattern#>:
-            <#code#>
-        default:
-            <#code#>
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
         }
     }
+    @IBAction func key_3(sender: AnyObject) {
+    //Result.text = "3"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    @IBAction func key_4(sender: AnyObject) {
+    //Result.text = "4"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    @IBAction func key_5(sender: AnyObject) {
+    //Result.text = "5"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    @IBAction func key_6(sender: AnyObject) {
+    //Result.text = "6"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    @IBAction func key_7(sender: AnyObject) {
+    //Result.text = "7"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    @IBAction func key_8(sender: AnyObject) {
+    //Result.text = "8"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    @IBAction func key_9(sender: AnyObject) {
+    //Result.text = "9"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    @IBAction func key_0(sender: AnyObject) {
+    //Result.text = "0"
+        let key = sender.currentTitle
+        if (DisplayWindowAcknowledgement){
+            Result.text = Result.text! + key!!
+        } else{
+            Result.text = key!
+            if (key != "0"){
+                DisplayWindowAcknowledgement = true
+            }
+        }
+    }
+    
+    
+
     @IBAction func Add(_ sender: Any) {
     }
     @IBAction func Minus(_ sender: Any) {
@@ -74,9 +144,8 @@ class ViewController: UIViewController {
     }
     @IBAction func Clear(_ sender: Any) {
     }
-    
-  
-    
+    @IBAction func Equal(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
