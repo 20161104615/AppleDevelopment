143e72299c2e43dfa13202d6952139df9330f57f
20161104615 <20161104615a@admindeiMac-9.local>
2018-10-12 17:38:47 +0800

    Update

--- Calculator/Calculate2/Calculate2/ViewController.swift
+++ Calculator/Calculate2/Calculate2/ViewController.swift
@@ -11,42 +11,62 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    
-    
-    
+    var n1 = Int()
+    var n2 = Int()
+    var logo = String()
+
     @IBOutlet weak var display: UILabel!
-    var FirstTime = false
+    
+    var FirstTime = false//判断当前是否为0
     
     @IBAction func key(_ sender: UIButton) {
         let digit = sender.currentTitle
         if FirstTime{
-            let digit_Old = display.text!
-            display.text = digit_Old + digit!
+            let textCurrent = display.text!
+            display.text = textCurrent + digit!
         } else {
             display.text = digit
             FirstTime = true
         }
     }
     
+    @IBAction func add(_ sender: UIButton) {
+        n1 = Int(display.text!)!
+        display.text = ""
+        logo = "+"
+    }
     
+    @IBAction func minus(_ sender: UIButton) {
+        n1 = Int(display.text!)!
+        display.text = ""
+        logo = "-"
+    }
+    
+    @IBAction func ride(_ sender: UIButton) {
+        n1 = Int(display.text!)!
+        display.text = ""
+        logo = "*"
+    }
+    
+    @IBAction func except(_ sender: UIButton) {
+        n1 = Int(display.text!)!
+        display.text = ""
+        logo = "/"
+    }
     @IBAction func Calculator(_ sender: UIButton) {
-        FirstTime = false
-        if let number1 = sender.currentTitle {
-            let number2 = String()
-            
-            switch number1 {
-            case "+":
-                display.text = number2
-            case "-":
-                
-            default:
-                break
-            }
-            
-            
+        n2 = Int(display.text!)!
+        switch logo {
+        case "+":
+            display.text = String((n1 + n2))
+        case "-":
+            display.text = String((n1 - n2))
+        case "*":
+            display.text = String((n1 * n2))
+        case "/":
+            display.text = String((n1 / n2))
+        default:
+            break
         }
-        
-        
     }
     
     
