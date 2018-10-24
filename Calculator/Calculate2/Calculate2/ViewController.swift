adf36b7777998dc9eab1dd92f10ed371a7540243
20161104615 <20161104615a@admindeiMac-9.local>
2018-10-24 11:11:36 +0800

    UpdateContinuity
    连续计算

--- Calculator/Calculate2/Calculate2/ViewController.swift
+++ Calculator/Calculate2/Calculate2/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
     var n1 = Double()
     var n2 = Double()
     var logo = String()
-    var count = 0
+    var CountAdd = 0
+    var CountMinus = 0
+    var CountRide = 0
+    var CountExcept = 0
 
     @IBOutlet weak var display: UILabel!
     
@@ -32,36 +35,63 @@ class ViewController: UIViewController {
     }
     
     @IBAction func add(_ sender: UIButton) {
-        if (count >= 0){
+        if (CountAdd == 0){
             n1 = Double(display.text!)!
             display.text = ""
             logo = "+"
-            
-        } else{
-            n1 = Double(display.text!)!
-            display.text = ""
-            logo = "+"
-            
+            CountAdd += 1
         }
-        
+        else{
+            n1 += Double(display.text!)!
+            display.text = ""
+            logo = "+"
+            CountAdd += 1
+        }
     }
     
     @IBAction func minus(_ sender: UIButton) {
-        n1 = Double(display.text!)!
-        display.text = ""
-        logo = "-"
+        if (CountMinus == 0){
+            n1 = Double(display.text!)!
+            display.text = ""
+            logo = "-"
+            CountMinus += 1
+        }
+        else{
+            n1 -= Double(display.text!)!
+            display.text = ""
+            logo = "-"
+            CountMinus += 1
+        }
     }
     
     @IBAction func ride(_ sender: UIButton) {
-        n1 = Double(display.text!)!
-        display.text = ""
-        logo = "*"
+        if (CountRide == 0){
+            n1 = Double(display.text!)!
+            display.text = ""
+            logo = "*"
+            CountRide += 1
+        }
+        else{
+            n1 *= Double(display.text!)!
+            display.text = ""
+            logo = "*"
+            CountRide += 1
+        }
     }
     
     @IBAction func except(_ sender: UIButton) {
-        n1 = Double(display.text!)!
-        display.text = ""
-        logo = "/"
+        if (CountExcept == 0){
+            n1 = Double(display.text!)!
+            display.text = ""
+            logo = "/"
+            CountExcept += 1
+        }
+        else{
+            n1 /= Double(display.text!)!
+            display.text = ""
+            logo = "/"
+            CountExcept += 1
+        }
     }
     @IBAction func Calculator(_ sender: UIButton) {
         n2 = Double(display.text!)!
@@ -83,6 +113,10 @@ class ViewController: UIViewController {
         n1 = 0
         n2 = 0
         logo = ""
+        CountAdd = 0
+        CountMinus = 0
+        CountRide = 0
+        CountExcept = 0
         display.text = ""
     }
     
