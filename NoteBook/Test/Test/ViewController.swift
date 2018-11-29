5714825c36e8bfadb9a55341ee7f7908254d3e04
20161104615 <ys1019172727@vip.qq.com>
2018-11-29 18:10:20 +0800

    Update

--- NoteBook/Test/Test/ViewController.swift
+++ NoteBook/Test/Test/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  Note
+//  Test
 //
-//  Created by 20161104615 on 2018/11/28.
+//  Created by 20161104615 on 2018/11/29.
 //  Copyright © 2018 20161104615. All rights reserved.
 //
 
@@ -13,13 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
         menuButton.target = self.revealViewController()
         menuButton.action = #selector(revealViewController()?.revealToggle(_:))
-        
-        self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
