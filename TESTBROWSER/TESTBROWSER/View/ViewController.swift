89e855bc980a9f371dde6b74d4f935b613cda340
20161104615 <ys1019172727@vip.qq.com>
2019-01-04 11:32:20 +0800

    UpdateBrowser

--- TESTBROWSER/TESTBROWSER/View/ViewController.swift
+++ TESTBROWSER/TESTBROWSER/View/ViewController.swift
@@ -14,12 +14,9 @@ class ViewController: UIViewController,UIWebViewDelegate,UITextFieldDelegate {
     @IBOutlet weak var progress: UIProgressView!
     @IBOutlet weak var url: UITextField!
     @IBOutlet weak var webview: UIWebView!
-//    var loadIndicator:UIActivityIndicatorView!
-//    //进度条计时器
-//    var ptimer:Timer!
-    
+
     @IBAction func buttonClick(_ sender: Any) {
-        var color = 0
+        _ = 0
         self.webview.loadRequest(NSURLRequest(url: NSURL(string: "http://" + url.text!)! as URL) as URLRequest)
         url!.resignFirstResponder()
         
@@ -49,80 +46,5 @@ class ViewController: UIViewController,UIWebViewDelegate,UITextFieldDelegate {
         url.minimumFontSize = 10
         progress.progress = 0
     }
-    
-//
-//    func setupBrowserToolbar(){
-//        //创建计时器对象
-//        ptimer = Timer.scheduledTimer(timeInterval: 0.2, target:self,
-//                                      selector: #selector(ViewController().loadProgress()),
-//                                      userInfo:nil,repeats:true)
-//        ptimer.invalidate()
-//    }
-//
-//
-//
-//    func loadUrl(_ url:String){
-//        let urlobj = URL(string:url)
-//        let request = URLRequest(url:urlobj!)
-//        webview.loadRequest(request);
-//    }
-//
-//    func stopClicked(_ sender:UIBarButtonItem){
-//        webview.stopLoading()
-//    }
-//
-//    func reloadClicked(_ sender:UIBarButtonItem){
-//        webview.reload()
-//    }
-//
-//    func backClicked(_ sender:UIBarButtonItem){
-//        webview.goBack()
-//    }
-//
-//    func forwardClicked(_ sender:UIBarButtonItem){
-//        webview.goForward()
-//    }
-//
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//    }
-//
-//    func webViewDidStartLoad(_ webView:UIWebView){
-//        progBar.setProgress(0, animated:false);
-//        ptimer.fire();
-//        loadIndicator.startAnimating();
-//    }
-//    func webViewDidFinishLoad(_ webView:UIWebView){
-//        loadIndicator.stopAnimating();
-//        progBar.setProgress(1, animated:true);
-//        ptimer.invalidate();
-//    }
-//    func loadProgress(){
-//        // 如果进度满了，停止计时器
-//        if(progBar.progress >= 1.0){
-//            // 停用计时器
-//            ptimer.invalidate();
-//        } else {
-//            // 改变进度条的进度值
-//            progBar.setProgress(progBar.progress + 0.02, animated:true);
-//        }
-//    }
-//
-//    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-//        //999错误过滤(防止页面正在加载时候，点击stop按钮，提示NSURLErrorDomain error=-999)
-//        if error._code == NSURLErrorCancelled {
-//            return
-//        }
-//
-//        let alertController = UIAlertController(title: "出错!",
-//                                                message: error.localizedDescription,
-//                                                preferredStyle: .alert)
-//        let okAction = UIAlertAction(title: "确定", style: .cancel, handler: nil)
-//        alertController.addAction(okAction)
-//        self.present(alertController, animated: true, completion: nil)
-//    }
-
-
 }
 
